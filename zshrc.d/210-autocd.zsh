2b96a4aeabd6c96758f4c0e39bd67485c2dd9c74
Ariel De Ocampo <arielmakestuff@gmail.com>
2016-01-17 15:29:53 -0500

    zshrc: cleanup 210-autocd.zsh

--- zshrc.d/210-autocd.zsh
+++ zshrc.d/210-autocd.zsh
@@ -2,16 +2,10 @@
 # Home locations
 # -----------------------------------------------------------------
 # Basic
-docs=$HOME/opt/documents
-shared=$HOME/opt/shared
 tmp=$HOME/tmp
 
-# Personal
-downloads=$HOME/opt/downloads
+# Personal downloads=$HOME/opt/downloads
 torrentdl=$HOME/opt/downloads/torrents
-torrentfiles=$HOME/tmp/torrents
-pictures=$HOME/usr/share/pictures
-finance=$HOME/usr/share/finances
 music=$HOME/opt/music
 games=$HOME/opt/games
 projects=$HOME/opt/projects
@@ -22,41 +16,28 @@ dev=$HOME/opt/projects/devel
 programming=$HOME/usr/src
 repos=$HOME/opt/repos
 hgrepos=$HOME/opt/repos/hg
-hgarchives=$HOME/opt/repos/hg/programming/archives/current
 hgwork=$HOME/opt/repos/hg/programming/work
-hgconfig=$HOME/opt/repos/hg/config
 gitrepos=$HOME/opt/repos/git
-gitarchives=$HOME/opt/repos/git/programming/archives/current
 gitwork=$HOME/opt/repos/git/programming/work
-gitconfig=$HOME/opt/repos/git/config
-svnwork=$HOME/opt/repos/svn/programming/work
-#hgtailor=$HOME/opt/repos/hg/programming/tailor
-#hg2svn=$HOME/opt/repos/hg/programming/svn
 
 # Git
 gitpub=$HOME/opt/repos/git/public
 gitpriv=$HOME/opt/repos/git/private
 gitbare=$HOME/opt/repos/git/export
 
-# App specific
-kde=$HOME/.config/local/kde/kde
-
 # -----------------------------------------------------------------
 # Non-Home locations
 # -----------------------------------------------------------------
 usrdoc=/usr/share/doc
-aptrepo=/home/apt/repo
 
 # -----------------------------------------------------------------
 # Set aliases
 # -----------------------------------------------------------------
-: ~downloads ~docs ~shared ~projects ~dev ~pydev \
-~tmp ~programming ~repos \
-~hgconfig ~hgrepos \
-~hgarchives ~hgwork \
-~gitconfig ~gitrepos \
-~gitarchives ~gitwork \
-~gitpub ~gitpriv ~gitbare \
-~torrentfiles ~torrentdl \
-~pictures ~finance ~kde ~usrdoc \
-~aptrepo ~music ~games
+
+: ~tmp downloads ~torrentdl ~music ~games ~projects ~pydev ~dev \
+~programming ~repos ~hgrepos ~hgwork ~gitrepos ~gitwork ~gitpub \
+~gitpriv ~gitbare ~usrdoc
+
+# -----------------------------------------------------------------
+#
+# -----------------------------------------------------------------
