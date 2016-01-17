e43e3db25dee190976440dbd9b4b76772e307d03
Ariel De Ocampo <arielmakestuff@gmail.com>
2016-01-17 11:28:22 -0500

    Update personal settings

--- zshenv.d/090-personal.zsh
+++ zshenv.d/090-personal.zsh
@@ -3,13 +3,23 @@
 # =================================================================
 
 
-# Mutt/email stuff
+# Personal preferences
 export EMAIL='Ariel De Ocampo <arieldeocampo@gmail.com>'
-export EDITOR=$HOME/usr/bin/vim
+# export EDITOR=$HOME/usr/bin/nvim
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=$XDG_CACHE_HOME/zsh/history
+VISUAL=nvim
+EDITOR=$VISUAL
 
 # NVIM stuff
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
 
+# Path
+export PATH=$HOME/usr/bin:$PATH
+
+# Password Store
+export PASSWORD_STORE_DIR=$HOME/.local/share/local/password-store
 
 # =================================================================
 #
