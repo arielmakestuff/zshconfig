e197c180c59dc0f5059f7339d9361e2d3db61ad8
Ariel De Ocampo <arielmakestuff@gmail.com>
2016-12-31 22:51:06 -0500

    Add zshenv config for rust

---
+++ zshenv.d/040-rust.zsh
@@ -0,0 +1,17 @@
+# =================================================================
+# Rust settings
+# =================================================================
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:~/usr/stow/rust-1.10/lib"
+
+export CARGO_HOME=$XDG_DATA_HOME/cargo
+
+export RUST_SRC_PATH=$HOME/opt/repos/git/public/rust/src
+
+# Path
+path=($XDG_DATA_HOME/cargo/bin $path)
+
+
+# =================================================================
+#
+# =================================================================
