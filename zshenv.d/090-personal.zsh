ca77b97b37df27293e68ae5e0fff2fc9489cfd98
Ariel De Ocampo <arielmakestuff@gmail.com>
2018-01-26 18:01:09 -0500

    Remove obsolete env var NVIM_TUI_ENABLE_TRUE_COLOR
    Neovim no longer uses this env variable.

--- zshenv.d/090-personal.zsh
+++ zshenv.d/090-personal.zsh
@@ -11,17 +11,14 @@ HISTSIZE=1000
 SAVEHIST=1000
 HISTFILE=$XDG_CACHE_HOME/zsh/history
 
-# NVIM stuff
-export NVIM_TUI_ENABLE_TRUE_COLOR=1
-
 # Path is set in zprofile.d/000-path.zsh
 # path=($HOME/.local/bin $path)
-# 
+#
 # # Python virtualenv project paths
 # if [ -n $VENV_PROJECT_DIR ]; then
 #     path=($VENV_PROJECT_DIR $path)
 # fi
-# 
+#
 # Password Store
 export PASSWORD_STORE_DIR=$XDG_DATA_HOME/local/password-store
 
