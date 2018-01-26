7bd9d534f550f4c71afd3cee6c3746ab087ec450
Ariel De Ocampo <arielmakestuff@gmail.com>
2018-01-26 17:59:26 -0500

    Don't set TERM var, instead set COLORTERM var
    Since using alacritty and tmux, setting the TERM var messes things up.
    Instead, in order to use true color, the env var COLORTERM is set.

--- zshenv.d/060-term.zsh
+++ zshenv.d/060-term.zsh
@@ -7,7 +7,10 @@
 if [[ -z $DISPLAY ]]; then
     export TERM=linux
 else
-    export TERM=rxvt-unicode-256color
+    # export TERM=rxvt-unicode-256color
+    # export TERM=screen-256color
+    # export TERM=tmux-256color
+    export COLORTERM=truecolor
 fi
 
 
