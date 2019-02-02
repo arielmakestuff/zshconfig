d72fdd358161f42a0745b96d8497ad154a989afb
Ariel De Ocampo <arielmakestuff@gmail.com>
2019-02-02 11:59:24 -0500

    refactor: remove commented out code

--- zshenv.d/060-term.zsh
+++ zshenv.d/060-term.zsh
@@ -3,13 +3,9 @@
 # =================================================================
 
 
-# export TERM=xterm-256color
 if [[ -z $DISPLAY ]]; then
     export TERM=linux
 else
-    # export TERM=rxvt-unicode-256color
-    # export TERM=screen-256color
-    # export TERM=tmux-256color
     export COLORTERM=truecolor
 fi
 
