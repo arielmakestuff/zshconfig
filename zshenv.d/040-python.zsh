22aefa21c8c53e670b4e9bdc1186aee7730a0255
Ariel De Ocampo <arielmakestuff@gmail.com>
2019-02-24 20:02:57 -0500

    Update python env vars w/ new dirs for pipx and venv

--- zshenv.d/040-python.zsh
+++ zshenv.d/040-python.zsh
@@ -7,13 +7,13 @@
 export DEVPI_SERVERDIR=$XDG_DATA_HOME/devpi
 
 # virtualenvwrapper
-VENV_DIR=$XDG_DATA_HOME/pipx/venv/virtualenvwrapper
+VENV_DIR=$XDG_DATA_HOME/pipx/venvs/virtualenvwrapper
 export VIRTUALENVWRAPPER_PYTHON=$VENV_DIR/bin/python
 export WORKON_HOME=$XDG_DATA_HOME/python-venv
 export VIRTUALENVWRAPPER_HOOK_DIR=$WORKON_HOME
 
 # pipx
-export PIPX_HOME=$XDG_DATA_HOME/pipx/venv
+export PIPX_HOME=$XDG_DATA_HOME/pipx
 
 # Default python version
 export PYTHONVERSION=3.6
