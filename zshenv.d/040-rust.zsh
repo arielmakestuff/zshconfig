10ed014c7a0999184576ab72184fc9627250ad71
Ariel De Ocampo <arielmakestuff@gmail.com>
2019-05-02 19:19:14 -0400

    Add rust env var: RUSTC_WRAPPER
    Rust's cargo tool can share build artifacts among all packages in a
    single workspace, but not across different workspaces. This means that
    it is possible for there to be multiple copies of the same build
    artifacts from different projects that depend on the same crates.
    
    This change adds the RUSTC_WRAPPER env variable set to the `sccache`
    tool. This tool allows sharing build results across different
    workspaces.

--- zshenv.d/040-rust.zsh
+++ zshenv.d/040-rust.zsh
@@ -9,6 +9,10 @@ export CARGO_HOME=$XDG_DATA_HOME/cargo
 
 export RUST_SRC_PATH=$RUSTUP_HOME/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
 
+if [ -x "$(command -v sccache)" ]; then
+    export RUSTC_WRAPPER=sccache
+fi
+
 # Path is set to zprofile.d/000-path.zsh
 # path=($CARGO_HOME/bin $path)
 
